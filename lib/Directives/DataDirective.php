114c56a8e794131a6e7c9f6e0895b11d2a411819
lina.wolf <lwolf@w-commerce.de>
2023-01-10 20:43:36 +0000

    Happyfy ci

--- lib/Directives/DataDirective.php
+++ lib/Directives/DataDirective.php
@@ -8,39 +8,34 @@ use Doctrine\RST\Directives\Directive;
 use Doctrine\RST\Nodes\Node;
 use Doctrine\RST\Parser;
 
+use function htmlspecialchars;
+
 class DataDirective extends Directive
 {
     private string $name;
     private string $templateName;
 
-    /**
-     * @param string $name
-     */
     public function __construct(string $name, ?string $templateName = null)
     {
-        $this->name = $name;
-        $this->templateName = $templateName??'directive/' . $this->name . '.html.twig';
+        $this->name         = $name;
+        $this->templateName = $templateName ?? 'directive/' . $this->name . '.html.twig';
     }
 
     public function processNode(Parser $parser, string $variable, string $data, array $options): ?Node
     {
-        $renderedCode = $parser->renderTemplate($this->templateName,
+        $renderedCode = $parser->renderTemplate(
+            $this->templateName,
             ['data' => htmlspecialchars($data)]
         );
+
         return $parser->getNodeFactory()->createRawNode($renderedCode);
     }
 
-    /**
-     * @return string
-     */
     public function getName(): string
     {
         return $this->name;
     }
 
-    /**
-     * @param string $name
-     */
     public function setName(string $name): void
     {
         $this->name = $name;
